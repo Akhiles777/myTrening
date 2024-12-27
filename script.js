626cc499626ed2e51964f610f27e3ac537ba01bb
Gasan <gasan07.03.2009@icloud.com>
2024-12-27 23:36:48 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,19 @@
+
+const scrollToTopButton = document.getElementById('scrollToTop');
+
+
+window.addEventListener('scroll', () => {
+    if (window.scrollY > 300) {
+        scrollToTopButton.classList.add('show');
+    } else {
+        scrollToTopButton.classList.remove('show');
+    }
+});
+
+
+scrollToTopButton.addEventListener('click', () => {
+    window.scrollTo({
+        top: 0,
+        behavior: 'smooth'
+    });
+});
